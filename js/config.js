a3c28ceaa4497eabfa528d2d02c8a4b77361ccc1
Armaan <armaan@example.com>
2026-05-11 07:36:32 +0530

    Updated Supabase credentials

--- js/config.js
+++ js/config.js
@@ -2,8 +2,8 @@
 // Replace these with your actual Supabase URL and Anon Key
 // On Vercel, you can use build-time replacement or just update this file
 const CONFIG = {
-    SUPABASE_URL: 'https://zukendvdmfnpfuzgamgq.supabase.co',
-    SUPABASE_ANON_KEY: 'sb_publishable_gu95WRM4_bnPTq3kSXgM9A_txPWnrps'
+    SUPABASE_URL: 'https://ukqhjqytfsuxycunwrfc.supabase.co',
+    SUPABASE_ANON_KEY: 'sb_publishable_iie9veVYH4e8anXowQ_LsQ_RWfVyLLf'
 };
 
 export default CONFIG;
